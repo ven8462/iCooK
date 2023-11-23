f73ca1f236c918aa04cc64bea427b33a10d302c0
lavender <lavenderanyango9@gmail.com>
2023-11-24 00:59:41 +0300

    database set up

--- app/app.py
+++ app/app.py
@@ -1,20 +1,48 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 posts = [
     {
         "author": "Lavender",
         "title": "Chapo Beans",
-        "ingredients": ["flour", "beans"],
+        "ingredients": ["chapo", "beans"],
         "method": ["Make chapos", "cook beans", "serve while hot"],
         "date_posted": "Nov 20, 2023",
         "date_updated": "Nov 22, 2023",
     },
     {
-        "author": "Jane Doe",
+        "author": "Ven",
         "title": "Ugali Beef",
         "ingredients": ["flour", "water", "beef", "onions", "tomatoes", "oil", "salt", "dhania"],
         "method": ["Make ugali", "cook beef", "serve while hot"],
@@ -22,6 +50,7 @@ posts = [
         "date_updated": "Oct 22, 2023",
     },
 ]
+
 
 @app.route("/")
 @app.route("/home")
