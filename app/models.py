2091ca9a71b8369ad10e185cbb01586e7d8dc5c9
lavender <lavenderanyango9@gmail.com>
2023-12-01 19:35:52 +0300

    my branch

--- app/models.py
+++ app/models.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from app import db, login_manager, app
 from flask_login import UserMixin
-
+from app import db, login_manager, app
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +13,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    favorites = db.relationship("Favorite", backref="user", lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
@@ -29,6 +28,16 @@ class User(db.Model, UserMixin):
             return None
         return User.query.get(user_id)
 
+    def get_favorites(self):
+        return Favorite.query.filter_by(user_id=self.id).all()
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=True)
+    source = db.Column(db.String(255), nullable=True)
+    url = db.Column(db.String(255), nullable=True)
--- app/routes.py
+++ app/routes.py
@@ -5,11 +5,12 @@ from flask import jsonify, render_template, url_for, flash, redirect, request
 from app.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
                              RequestResetForm, ResetPasswordForm)
 from app import app, db, bcrypt
-from app.models import User
+from app.models import User, Favorite
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_mail import Message, Mail
 from app.recipe import get_recipe_details
 import requests
+from urllib.parse import urlparse
 
 
 @app.route('/')
@@ -40,7 +41,46 @@ def recipe(recipe_id):
 
    return render_template('recipe.html', recipe=recipe_dict)
 
+   
+@app.route("/add-to-favorites", methods=["POST"])
+def add_to_favorites():
+    if request.method == "POST":
+        recipe_id = request.form.get("recipe_id")
 
+        # Fetch recipe details using the recipe_id (similar to your /recipe/<recipe_id> route)
+        url = f"https://api.edamam.com/api/recipes/v2/{recipe_id}"
+        params = {
+            "type": "public",
+            "app_id": "5f8d15e8",
+            "app_key": "7e4f94d1f57158c014144b6f0864dc56",
+        }
+        
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            recipe_details = response.json().get("recipe")
+            if recipe_details:
+                # Create a Favorite object and save it to the database
+                favorite = Favorite(
+                    user_id=current_user.id,  # Assuming you're using Flask-Login for authentication
+                    title=recipe_details.get("label"),
+                    image=recipe_details.get("image"),
+                    source=recipe_details.get("source"),
+                    url=recipe_details.get("url"),
+                )
+
+                db.session.add(favorite)
+                db.session.commit()
+
+                # Redirect the user back to the recipe page
+                return redirect(url_for('recipe', recipe_id=recipe_id))
+            else:
+                flash('Error fetching recipe details from the API', 'error')
+        else:
+            flash('Error in API request. Status Code: {}'.format(response.status_code), 'error')
+
+        # Redirect the user back to the recipe page
+        return redirect(url_for('recipe', recipe_id=recipe_id))
 
 
 @app.route("/about")
@@ -166,3 +206,10 @@ def reset_token(token):
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
 
+@app.route('/favorites')
+def favorites():
+   # Get the favorites of the current user
+   favorites = current_user.get_favorites() if current_user.is_authenticated else []
+
+   # Pass the favorites to the template
+   return render_template('favorites.html', favorites=favorites)
